5a6d424fb328a505086529713ff8de88fbd0ca52
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2021-06-03 14:22:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.prefix}-${var.name}"
-  acl    = "public-read"
+  acl    = "private"
 
   policy = <<EOF
 {
@@ -34,7 +34,7 @@ EOF
 }
 
 resource "aws_s3_bucket_object" "webapp" {
-  acl          = "public-read"
+  acl          = "private"
   key          = "index.html"
   bucket       = aws_s3_bucket.bucket.id
   content      = file("${path.module}/assets/index.html")
